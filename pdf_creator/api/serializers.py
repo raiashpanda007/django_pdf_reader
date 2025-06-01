5cdc12d09bf3e5b1ea6f80e9144425835adc297c
Ashwin <ashwin.2201098cs@iiitbh.ac.in>
2025-06-02 01:05:57 +0530

    Add LoginSerializer for user authentication and refactor imports in serializers.py

--- pdf_creator/api/serializers.py
+++ pdf_creator/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from django.contrib.auth.models import get_user_model
+from django.contrib.auth import authenticate
 from .models import CustomUser
 
 
@@ -17,3 +17,16 @@ class Signup(serializers.ModelSerializer):
         }
     def create(self, validated_data):
         return CustomUser.objects.create_user(**validated_data)
+    
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(email=data['email'], password=data['password'])
+        if not user:
+            raise serializers.ValidationError("Invalid credentials")
+        if not user.is_active:
+            raise serializers.ValidationError("User account is disabled")
+        data['user'] = user
+        return data
