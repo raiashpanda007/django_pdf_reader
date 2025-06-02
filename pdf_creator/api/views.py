6e5ecb7baae57642f741f799b106a1687e1512d8
Ashwin <ashwin.2201098cs@iiitbh.ac.in>
2025-06-02 07:44:38 +0530

    Implement SignupView for user registration with validation and response handling

--- pdf_creator/api/views.py
+++ pdf_creator/api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import CustomUser
+from .serializers import Signup
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+class SignupView(APIView):
+    def post (self,request):
+        serializer = Signup(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
